2ca4d85a0acb45b2762e0e6498e06eef12911428
Armando Robles <arobles.us@gmail.com>
2019-09-19 11:37:21 -0400

    Untested

--- queries.js
+++ queries.js
@@ -1,33 +1,51 @@
 /* Add all the required libraries*/
+var mongoose = require('mongoose');
 
-/* Connect to your database using mongoose - remember to keep your key secret*/
+mongoose.connect('mongodb+srv://arobles:Ypj0OlM2wV5LWSGD@bootcamp-db-ogvpo.azure.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser : true});
 
 /* Fill out these functions using Mongoose queries*/
-//Check out - https://mongoosejs.com/docs/queries.html
-
 var findLibraryWest = function() {
-  /* 
+  /*
     Find the document that contains data corresponding to Library West,
-    then log it to the console. 
    */
+    Listing.findOne( {'name' : 'Library West'}, function(err, listing){
+        if (err) throw err;
+
+        console.log("Found listing:\n%s", JSON.stringify(listing));
+    });
 };
+
 var removeCable = function() {
   /*
-    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed 
-    on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
-    and remove this listing from your database and log the document to the console. 
+    Find the document with the code 'CABL'. Remove this listing from database.
    */
+    Listing.findOneAndDelete( {'code' : 'CABL'}, function(err, listing){
+        if (err) throw err;
+
+        console.log("Deleted listing:\n%s", JSON.stringify(listing));
+    });
 };
+
 var updatePhelpsMemorial = function() {
   /*
-    Phelps Memorial Hospital Center's address is incorrect. Find the listing, update it, and then 
-    log the updated document to the console. 
+    Phelps Memorial Hospital Center's address is incorrect. Update.
    */
+    Listing.findOneAndUpdate( {'code' : 'PHL'}, {'address' : '1953 Museum Rd, Gainesville, FL 32603'}, function(err, listing){
+       if (err) throw err;
+
+        console.log("Updated listing:\n%s", JSON.stringify(listing));
+    });
 };
+
 var retrieveAllListings = function() {
-  /* 
-    Retrieve all listings in the database, and log them to the console. 
-   */
+    /*
+        Retrieve all listings in the database, and log them to the console.
+    */
+    Listing.find({}, function(err, listing){
+        if (err) throw err;
+
+        console.log(JSON.stringify(listing));
+    });
 };
 
 findLibraryWest();
